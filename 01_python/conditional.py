75e493e3f1b9a6f8563770e658284b14ea147beb
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-03 01:14:45 +0530

    Add conditional.py with examples of if-else statements

---
+++ 01_python/conditional.py
@@ -0,0 +1,20 @@
+a = 10 
+b = 20
+
+if a > b : 
+    print("lolo")
+print("noob")
+
+if a >= 90 : 
+    print("good")
+else :
+    print("noob")
+print("thankyou")
+
+
+if b >= 50 : 
+    print("good")
+elif b >= 30 and b < 50 :
+    print("ok ok")
+else :
+    print("noob")
