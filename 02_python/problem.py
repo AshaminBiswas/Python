8f982231f7f88fca43784eafa9236d14f73466a7
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-07 02:28:41 +0530

    Add problem.py with commented examples for even numbers, squares, odd sums, divisibility, and billing system

---
+++ 02_python/problem.py
@@ -0,0 +1,36 @@
+#write a program to find of all even number upto 50
+
+# sum = 0
+# for i in range(1,51) : 
+#     if i % 2 == 0 :
+#         sum += i
+# print(sum)
+
+
+
+#write a program to write first 20 numbers and their squired numbers
+
+# for i in range(1,21) :
+#     print("number is : ", i, "Squire is : ", i**2)
+
+
+
+#write a program to find sum of odd number  upto 10
+# sum = 0
+# n = 1
+# while n <= 20 :
+#     if n % 2 == 1 :
+#      sum+=n
+#     n+=1
+# print(sum)
+
+
+
+#write a program to check if a number is divisible by 8 and 12 upto 100
+
+# for i in range(1,101) : 
+#     if i % 8 == 0 and i % 12 == 0 :
+#         print(i)
+
+
+# write a billing system at supermarket
