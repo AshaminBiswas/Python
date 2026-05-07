8585dc284b2fe80ab07d51b10adb3c5bade8db79
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-08 01:15:13 +0530

    Add string manipulation examples in string.py

---
+++ 03_python/string.py
@@ -0,0 +1,18 @@
+a = "my name is ashamin biswas"
+
+print(len(a))
+
+#find a character how many times use in this variable
+print(a.count("a"))
+
+
+#convert lowercase and uppercase
+print(a.upper())
+
+#convert in to a title
+print(a.title())
+
+
+#write a program to find a index number of "Ashamin Biswas"
+
+print(a.find("ashamin biswas"))
