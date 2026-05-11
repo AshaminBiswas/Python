15cfc9602068f343c55ea07cdc52796bbc30940f
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-11 07:56:32 +0530

    Add list manipulation examples in list.py with sorting, reversing, and modifying elements

---
+++ Chapter_04/list.py
@@ -0,0 +1,27 @@
+# data = ["ashamin", 20, 15.22, "apple", "delhi"]
+# print(data)
+# data[2] = "noob"
+# print(data)
+
+# data[0:3]
+# print(data)
+
+data = [1,2,3,4,5,6,7,8,9]
+data.append(10)
+# print(data)
+
+l1 = [1,5,1,58,1,5,91,5,65,2,4,]
+l1.sort()
+print(l1)
+
+l1.reverse()
+print(l1)
+
+l1.insert(5, 5555)
+print(l1)
+
+l1.pop(5)
+print(l1)
+
+l1.remove(2)
+print(l1)
