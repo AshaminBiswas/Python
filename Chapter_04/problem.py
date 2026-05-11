3cdafc215fa467dda23fef2343fb82462014cc64
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-11 22:50:15 +0530

    Add initial implementation of problem.py with examples for fruit storage, student marks sorting, sum calculation, and tuple counting

---
+++ Chapter_04/problem.py
@@ -0,0 +1,36 @@
+#write a program to store seven fruits in a list enter by user
+# fruits = []
+# user = input("Enter the fruit name : ")
+# fruits.append(user)
+# print(fruits)
+
+
+
+#write a program to store five students marks and display in a sorted mnanar 
+
+# marks = []
+
+# s1 = int(input("Enter your marks : "))
+# marks.append(s1)
+# s2 = int(input("Enter your marks : "))
+# marks.append(s2)
+# s3 = int(input("Enter your marks : "))
+# marks.append(s3)
+# s4 = int(input("Enter your marks : "))
+# marks.append(s4)
+# s5 = int(input("Enter your marks : "))
+# marks.append(s5)
+# marks.sort()
+# print(marks)
+
+
+#make the the sum of all number in this list
+a = [1,52,8,9,45,12]
+
+print(sum(a))
+
+
+# write a program to count "0" in this following tuple
+
+tup = (0,4,5,0,58,0,5,0)
+print(tup.count(0))
