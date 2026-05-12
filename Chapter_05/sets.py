a58fd3d6d234b3c0bbfb46bfe890dca021f5264d
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-13 00:35:23 +0530

    Add initial implementation of sets.py with examples for set operations

---
+++ Chapter_05/sets.py
@@ -0,0 +1,13 @@
+e = set()
+set1 = {1,2,3,4,5,6,7,8,9}
+set2 = {1,5,78,1,995,6,8,76,5}
+
+# sets.add(555)
+# print(sets)
+
+# sets.remove(5)
+# print(sets)
+
+# print(set1.union(set2))
+
+print(set1.intersection(set2))
