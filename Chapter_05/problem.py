362b4dc6f9c5dd6eb0b7cc1b0ca5f4be19576ecb
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-13 01:00:34 +0530

    Add initial implementation of problem.py with examples for word lookup, number input, and dictionary updates

---
+++ Chapter_05/problem.py
@@ -0,0 +1,33 @@
+words = {
+    "madad" : "help",
+    "kalam" : "pen",
+    "kitab" : "book"
+}
+
+# word = input("Enter your word : ")
+# print(words.get(word))
+
+
+numbers = set()
+# numbers.add(int(input(" Enter your number")))
+# print(numbers)
+
+
+
+dic = {}
+
+# name = input("enter your name : ")
+# lang = input("enter language name : ")
+# dic.update({name:lang})
+# name = input("enter your name : ")
+# lang = input("enter language name : ")
+# dic.update({name:lang})
+# name = input("enter your name : ")
+# lang = input("enter language name : ")
+# dic.update({name:lang})
+# print(dic)
+
+
+
+
+
