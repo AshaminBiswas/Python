34d74aa8e6506078cae3cce5a6e2ca869f0d8d4b
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-15 01:31:34 +0530

    Add initial implementation of problem.py with user input examples for number comparison, percentage calculation, and spam detection

---
+++ Chapter_06/problem.py
@@ -0,0 +1,49 @@
+#Q1 ==>   find the greatest of four number entered my user
+
+
+# number1 = int(input("Enter your Number 1 : "))
+# number2 = int(input("Enter your Number 2 : "))
+# number3 = int(input("Enter your Number 3 : "))
+# number4 = int(input("Enter your Number 4 : "))
+
+# if(number1 > number2 and number1 > number3 and number1 > number4) : 
+#     print("Number1 is grater")
+# elif(number2 > number1 and number2 > number3 and number2 > number4) : 
+#     print("Number2 is grater")
+# elif(number3 > number1 and number3 > number2 and number3 > number4) : 
+#     print("Number3 is grater")
+# elif(number4 > number1 and number4 > number2 and number4 > number3) : 
+#     print("Number4 is grater")
+
+
+
+
+
+#Q2 ===>  #check total percentage  40 % and individual marks 33
+
+# marks1 = int(input("enter your marks 1 : "))
+# marks2 = int(input("enter your marks 2 : "))
+# marks3 = int(input("enter your marks 3 : "))
+
+
+# total_percentage = ((marks1 + marks2 + marks3)  / 300) * 100
+
+# if(total_percentage >= 40 and marks1 >= 33 and marks2 >= 33 and marks3 >= 33) :
+#     print("you are Pass.... ", total_percentage)
+# else : print("You fail", total_percentage)
+
+
+
+
+
+
+
+
+
+# Q3 ==> detect the spam text
+
+# word = input("enter your text : ")
+
+# if("spam" in word) : 
+#     print("this is a spam text")
+# else:print("it's safe")
