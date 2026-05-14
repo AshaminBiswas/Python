03e2e47f108a474d1f5889e9fa261ff808bab843
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-15 00:50:55 +0530

    Add initial implementation of if.py with age eligibility checks and comments

---
+++ Chapter_06/if.py
@@ -0,0 +1,18 @@
+a = int(input("Enter your age "))
+
+# if(a>=18) : 
+#     print("You are eligible")
+# else:print("you are ineligible")
+
+
+# if (a < 0) : 
+#     print("Bhai Tu payda he nahi hua")
+# elif(a >= 18) :
+#     print("ye hai mere gabru jawan")
+# else:print("tu nabalik hai")
+
+
+# if(a>=18):print("yes")
+# else:print("no")
+
+
