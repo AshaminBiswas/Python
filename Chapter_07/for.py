77e0267c86d1157535289f9efa3ec46b20ee12c1
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-15 22:36:08 +0530

    Add initial implementation of for.py with commented out loop examples

---
+++ Chapter_07/for.py
@@ -0,0 +1,23 @@
+# i = 1
+
+# for i in range(1,6):
+#     print(i)
+
+
+
+
+# l = ["ashamin", "biswas", "soumen", "hasan", "ushamin"]
+
+# for i in l :
+#     print(i)
+
+
+# t = (1, 1 , 3 , 4, 5)
+# for i in t :
+#     print(i)
+
+
+# s = "ashamin"
+# for i in s :
+#     print(i)
+# else:print("done ")
