27c9938d804af8de1a663e66e8730b123726ed39
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-15 22:25:00 +0530

    Add while loop implementation to print list contents in while.py

---
+++ Chapter_07/while.py
@@ -0,0 +1,17 @@
+
+# i = 1
+
+# while(i<51):
+#     print(i)
+#     i += 1
+
+
+
+#Q1, write a program to print the content of a list
+
+
+l = ["ashamin", "biswas", "soumen", "hasan", "ushamin"]
+i = 0
+while(i < len(l)):
+    print(l[i])
+    i += 1
