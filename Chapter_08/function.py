26bdacd0f7947e5dae0fa6313875d54a8ed855f9
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-17 22:43:10 +0530

    Uncomment factorial function and its invocation in function.py

--- Chapter_08/function.py
+++ Chapter_08/function.py
@@ -26,3 +26,15 @@ def average():
 #     print("good day,", name)
 #     print(ending)
 # goodDay("Ashamin")
+
+
+#recursion
+
+
+def factorial(n) : 
+    if(n==1 or n==0):
+        return 1
+    return n * factorial(n-1)
+
+n = int(input("enter number : "))
+print(f"factorial of {n} = {factorial(n)}")
