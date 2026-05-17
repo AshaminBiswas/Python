6142b0b8734a139081cd8796ac2871355b53f7e7
Ashamin Biswas <ashaminbiswas1@gmail.com>
2026-05-17 22:23:05 +0530

    Add average function implementation to calculate and print the average of three numbers

---
+++ Chapter_08/function.py
@@ -0,0 +1,23 @@
+
+#function definition
+def average():
+    a = int(input("Enter number one : "))
+    b = int(input("Enter number two : "))
+    c = int(input("Enter number three : "))
+    avg = (a+b+c)/3
+    print(avg)
+
+#function call
+# average()
+
+
+# def greet(buddy):
+#     print("Good day", buddy)
+#     return "done"
+# a = greet("Ashamin")
+# print(a)
+
+
+
+
+# default parameter
